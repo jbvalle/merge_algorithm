64d77d907242def138bcea426c682185c32f31af
John Bryan Valle <jbvalle.jv@gmail.com>
2021-12-28 01:25:55 +0100

    added comments

--- main.c
+++ main.c
@@ -1,16 +1,27 @@
+/* Algorithm for merging two array arrays of different sizes, which are ordered
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int *merge(int *arr1, int *arr2, int size1, int size2){
 
+    //Allocate memory for merged array
     int *merged = (int *)malloc((size1 + size2) * sizeof(int));
+    //Ternary Operator -> val = EXPRESSION1 ? EXPRESSION2: EXPRESSION3
+    //flag || {1,0} ... No changes -> Expression 1 of ternary Operator remains (arr1 < arr2)
+    //flag || {0,0} ... Sets Expression 1 to FALSE -> only Expression 3 will be run
+    //flag || {1,1} ... Sets Expression 1 to TRUE -> only Expression 2 will be run
     int state, flag[3][2] = {{1,0},{0,0},{1,1}};
 
     for(int i = 0, j = 0; (i < size1)||(j < size2);){
 
         merged[i + j - 1] = ((arr1[i] < arr2[j]) && flag[state][0]) || flag[state][1] ? arr1[i++]:arr2[j++]; 
-        
+        //If arr1 has reached its max then set EXP1 always to FALSE
+        //arr1 will not be incremented
         if(i == size1)state = 1;
+        //If arr2 has reached its max then set EXP1 always to TRUE
+        //arr2 will not be incremented
         if(j == size2)state = 2;
     }
 
